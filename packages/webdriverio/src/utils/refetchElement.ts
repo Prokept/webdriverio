fbf78471b2623254455b9a5127266596695f947a
Prokept <113397783+Prokept@users.noreply.github.com>
2022-09-14 15:36:06 -0700

    Update refetchElement.ts

--- packages/webdriverio/src/utils/refetchElement.ts
+++ packages/webdriverio/src/utils/refetchElement.ts
@@ -17,6 +17,7 @@ export default async function refetchElement (
     //Crawl back to the browser object, and cache all selectors
     while (currentElement.elementId && currentElement.parent) {
         await browser.pause(5000)
+        console.log("we did it!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         selectors.push({ selector: currentElement.selector, index: currentElement.index || 0 })
         currentElement = currentElement.parent as WebdriverIO.Element
     }
@@ -27,6 +28,7 @@ export default async function refetchElement (
     // Beginning with the browser object, rechain
     return selectors.reduce(async (elementPromise, { selector, index }, currentIndex) => {
         await browser.pause(5000)
+        console.log("we did it!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         const resolvedElement = await elementPromise
         let nextElement = index > 0 ? (await resolvedElement.$$(selector as string))[index] : null
         nextElement = nextElement || await resolvedElement.$(selector)
